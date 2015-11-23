17271a3decec73739ab79705d1fa7b51cb9afea5
Maciej Pochroń <m.pochron@stermedia.pl>
2015-11-23 10:09:16 +0100

    unfinished test jasmine

--- karma.conf.js
+++ karma.conf.js
@@ -5,10 +5,10 @@ module.exports = function(config){
 
     files : [
       'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/ui-router/release/angular-ui-router.min.js',
+      'app/bower_components/angular-resource/angular-resource.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/components/**/*.js',
-      'app/view*/**/*.js'
+      'app/js*/**/*.js'
     ],
 
     autoWatch : true,
